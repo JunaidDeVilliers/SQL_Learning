6fb9bce4ee09572d9316d15638caa536b4325265
Junaid <junaiddevilliers@gmail.com>
2025-02-15 17:09:34 +0200

    daily lessons

---
+++ 20. Order by/orderby.sql
@@ -0,0 +1,10 @@
+SELECT * FROM employees 
+ORDER BY last_name;
+
+-- The ORDER BY clause is used to sort the result set of a query by one or more columns.
+-- In this case, the result set will be sorted based on the specified column(s).
+-- You can sort the results in ascending order (ASC) or descending order (DESC).
+-- If no order is specified, the default is ascending order.
+-- Example:
+-- ORDER BY column1 ASC, column2 DESC;
+-- This will sort the results first by column1 in ascending order, and then by column2 in descending order.
